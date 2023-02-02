fa52fa6a21f88acc5a2f1c8834e8e089c80be6d1
BlackRain <masaraksh86@mail.ru>
2023-02-02 22:32:04 +0700

    Рефакторинг Использована интерполяция строк

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,12 @@ let expenses = "Питание: 725р. Проезд: 180р.";
 let purpose = 10000;
 let period = 9;
 
-console.log("money: " + typeof money + ", profit: " + typeof profit);
-console.log("expenses length: " + expenses.length);
-console.log("Период равен " + period + " месяцев");
-console.log("Цель заработать " + purpose + " рублей");
+console.log(`money: ${typeof money}, profit: ${typeof profit}`);
+console.log(`expenses length: ${expenses.length}`);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${purpose} рублей`);
 
 let budgetDay = money / 30;
-console.log("budgetDay: " + budgetDay + " рублей");
+console.log(`budgetDay: ${budgetDay} рублей`);
 
 console.log("Script \"main.js\" finished");

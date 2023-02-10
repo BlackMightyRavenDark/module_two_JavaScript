f60264e6dd9d8308a78647712c2cead30c6f9226
BlackRain <masaraksh86@mail.ru>
2023-02-10 13:07:57 +0700

    Выполнено д/з 02

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,63 @@
 
 console.log("Script \"main.js\" started");
 
-let money = 2500;
-let profit = "Фриланс: 500 рублей";
-let expenses = "Питание: 725р. Проезд: 180р.";
-let purpose = 10000;
-let period = 9;
+function showError() {
+    console.error("error");
+    document.writeln("Ошибка ввода!<br>");
+    alert("Ошибка ввода!");
+}
 
-console.log(`money: ${typeof money}, profit: ${typeof profit}`);
-console.log(`expenses length: ${expenses.length}`);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${purpose} рублей`);
+function roundToFixed(n, radix) {
+    if (typeof(n) === "number" && !Number.isNaN(n) && !Number.isInteger(n)) {
+        n = n.toFixed(radix);
+    }
+    return n;
+}
 
-let budgetDay = money / 30;
-console.log(`budgetDay: ${budgetDay} рублей`);
+let firstNumber = parseFloat(prompt("Введите первое число:"));
+if (!Number.isNaN(firstNumber)) {
+    const firstNumberString = Number.isInteger(firstNumber) ? firstNumber.toString() : firstNumber.toFixed(1);
+    let str = `Первое число: ${firstNumberString}`;
+    console.log(str);
+    document.writeln(`${str}<br>`);
+
+    let secondNumber = parseFloat(prompt("Введите второе число:"));
+    if (!Number.isNaN(secondNumber)) {
+        const secondNumberString = Number.isInteger(secondNumber) ? secondNumber.toString() : secondNumber.toFixed(1);
+        str = `Второе число: ${secondNumberString}`;
+        console.log(str);
+        document.writeln(`${str}<br>`);
+
+        const sum = roundToFixed(firstNumber + secondNumber, 1);
+        const difference = roundToFixed(firstNumber - secondNumber, 1);
+        const product = roundToFixed(firstNumber * secondNumber, 1);
+        const quotient = roundToFixed(Math.abs(secondNumber) >= 0.001 ? firstNumber / secondNumber : "error", 1);
+        const mod = roundToFixed(firstNumber % secondNumber, 1);
+
+        str = `${firstNumberString} + ${secondNumberString} = ${sum}`;
+        console.log(str);
+        document.writeln(`${str}<br>`);
+
+        str = `${firstNumberString} - ${secondNumberString} = ${difference}`;
+        console.log(str);
+        document.writeln(`${str}<br>`);
+
+        str = `${firstNumberString} * ${secondNumberString} = ${product}`;
+        console.log(str);
+        document.writeln(`${str}<br>`);
+
+        str = `${firstNumberString} / ${secondNumberString} = ${quotient}`;
+        console.log(str);
+        document.writeln(`${str}<br>`);
+
+        str = `${firstNumberString} % ${secondNumberString} = ${mod}`;
+        console.log(str);
+        document.writeln(`${str}<br>`);
+    } else {
+        showError();
+    }
+} else {
+    showError();
+}
 
 console.log("Script \"main.js\" finished");

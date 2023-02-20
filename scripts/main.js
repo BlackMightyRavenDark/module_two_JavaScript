b88c836a0fcc0f42643a93e38116dd68db2ae306
BlackRain <masaraksh86@mail.ru>
2023-02-20 18:49:32 +0700

    Выполнено д/з 04, часть 2

--- scripts/main.js
+++ scripts/main.js
@@ -15,4 +15,12 @@ for (let i = 0; i < week.length; ++i) {
     document.write(`День ${i + 1}: ${dayNameOutput}<br>`);
 }
 
+const arr = ["808", "753", "438", "345", "666", "777", "983", "327"];
+for (let i = 0; i < arr.length; ++i) {
+    const symbol = arr[i][0];
+    if (symbol === "3" || symbol === "7") {
+        console.log(arr[i]);
+    }
+}
+
 console.log("Script \"main.js\" finished");

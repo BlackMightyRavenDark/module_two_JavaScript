8cd1c2c4e3245fbf03bf2a5bb6eda30c6fb73698
BlackRain <masaraksh86@mail.ru>
2023-03-07 14:47:20 +0700

    Выполнено Д/З 08

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,6 @@
 
 console.log("Script \"main.js\" started");
 
-function showError() {
-    console.error("error");
-    document.writeln("Ошибка ввода!<br>");
-    alert("Ошибка ввода!");
-}
-
 function isValidNumber(n) {
     return typeof(n) === "number" && !Number.isNaN(n);
 }
@@ -32,61 +26,82 @@ const getTargetMonth = (targetProfit, monthIncome) => {
     return Math.ceil(targetProfit / monthIncome);
 }
 
-const money = parseFloat(prompt("Ваш месячный доход?"));
-if (isValidNumber(money)) {
+const program = () => {
+    let money;
+    do {
+        let moneyString = prompt("Ваш месячный доход?");
+        if (moneyString === null) return null;
+        money = parseFloat(moneyString);
+    } while (!isValidNumber(money));
+
     const profit = "фриланс, вёрстка, хакинг";
-    const extraMoney = parseFloat(prompt(`Ваш возможный доход за дополнительные работы (${profit}):`));
-    if (isValidNumber(extraMoney)) {
-        const expenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую:");
-        if (expenses !== "" && expenses.includes(",")) {
-            const amount = parseFloat(prompt("Во сколько обойдутся обязательные статьи расходов?"));
-            if (isValidNumber(amount)) {
-                const depositExists = confirm("Есть ли у вас вклад в банке? OK - Да, Cancel - Нет");
-                const accumulatedMonthIncome = getAccumulatedMonthIncome(money, extraMoney, amount);
-                if (accumulatedMonthIncome > 0) {
-                    logOutput(`Ваш бюджет на месяц с учётом ваших расходов составляет: ${roundToFixed(accumulatedMonthIncome, 1)}`);
 
-                    const incomeDay = accumulatedMonthIncome / 30;
-                    const incomeDayString = Number.isInteger(incomeDay) ? incomeDay.toString() : Math.floor(incomeDay);
-                    logOutput(`Дневной бюджет: ${incomeDayString}`);
+    let extraMoney;
+    do {
+        let extraMoneyString = prompt(`Ваш возможный доход за дополнительные работы (${profit}):`);
+        if (extraMoneyString === null) return null;
+        extraMoney = parseFloat(extraMoneyString);
+    } while (!isValidNumber(extraMoney));
 
-                    const purpose = parseFloat(prompt("Сколько вы хотите заработать?"));
-                    if (isValidNumber(purpose)) {
-                        if (purpose > accumulatedMonthIncome) {
-                            const arbeitenMonth = getTargetMonth(purpose, accumulatedMonthIncome);
-                            logOutput(`Ваша цель накопить ${purpose} с учётом всех ваших расхоодов будет достигнута за ${arbeitenMonth} месяцев`);
-                        } else {
-                            logOutput(`Вы уже достигли своей цели в ${purpose}!`);
-                        }
+    let expenses;
+    do {
+        expenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую:");
+        if (expenses === null) return null;
+    } while (!expenses.includes(","));
 
-                        if (incomeDay >= 6000) {
-                            logOutput("У вас высокий уровень дохода");
-                        } else if (incomeDay >= 3000) {
-                            logOutput("У вас средний уровень дохода");
-                        } else if (incomeDay > 0) {
-                            logOutput("У вас низкий уровень дохода");
-                        } else if (incomeDay === 0) {
-                            logOutput("Вы выходите в ноль");
-                        } else {
-                            logOutput("К сожалению, вы тратите больше, чем зарабатываете!");
-                        }
-                    } else {
-                        showError();
-                    }
-                } else {
-                    logOutput("К сожалению, Вы тратите больше, чем зарабатываете!");
-                }
-            } else {
-                showError();
-            }
-        } else {
-            showError();
-        }
-    } else {
-        showError();
+    let amount;
+    do {
+        let amountString = prompt("Во сколько обойдутся обязательные статьи расходов?");
+        if (amountString === null) return null;
+        amount = parseFloat(amountString);
+    } while (!isValidNumber(amount));
+
+    const isDepositExists = confirm("Есть ли у вас вклад в банке? OK - Да, Cancel - Нет");
+    logOutput(`Вклад в банке: ${isDepositExists ? "Есть" : "Нет"}`)
+
+    const accumulatedMonthIncome = getAccumulatedMonthIncome(money, extraMoney, amount);
+    if (accumulatedMonthIncome <= 0) {
+        logOutput("Цель не будет достигнута никогда! :'(");
+        return false;
     }
-} else {
-    showError();
+
+    logOutput(`Ваш бюджет на месяц с учётом ваших расходов составляет: ${roundToFixed(accumulatedMonthIncome, 1)}`);
+
+    const incomeDay = accumulatedMonthIncome / 30;
+    const incomeDayString = Number.isInteger(incomeDay) ? incomeDay.toString() : Math.floor(incomeDay);
+    logOutput(`Дневной бюджет: ${incomeDayString}`);
+
+    let purpose;
+    do {
+        let purposeString = prompt("Сколько вы хотите заработать?");
+        if (purposeString === null) return null;
+        purpose = parseFloat(purposeString);
+    } while (!isValidNumber(purpose));
+
+    if (purpose > accumulatedMonthIncome) {
+        const arbeitenMonth = getTargetMonth(purpose, accumulatedMonthIncome);
+        logOutput(`Ваша цель накопить ${purpose} с учётом всех ваших расхоодов будет достигнута за ${arbeitenMonth} месяцев`);
+    } else {
+        logOutput(`Вы уже достигли своей цели в ${purpose}!`);
+    }
+
+    if (incomeDay >= 6000) {
+        logOutput("У вас высокий уровень дохода");
+    } else if (incomeDay >= 3000) {
+        logOutput("У вас средний уровень дохода");
+    } else if (incomeDay > 0) {
+        logOutput("У вас низкий уровень дохода");
+    } else if (incomeDay === 0) {
+        logOutput("Вы выходите в ноль");
+    } else {
+        logOutput("Цель не будет достигнута никогда! :'(");
+    }
+
+    return true;
+}
+if (program() === null) {
+    logOutput("Вычисления были отменены!");
+    alert("Пользователь нажал отмену");
 }
 
 console.log("Script \"main.js\" finished");

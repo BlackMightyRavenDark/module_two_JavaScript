41ef177e4c52304cd43ecfdaeec079b2bf6bafb5
BlackRain <masaraksh86@mail.ru>
2023-03-27 18:03:42 +0700

    Исправлена последовательность глав

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
 const adv = document.querySelector(".adv");
 
 adv.parentNode.removeChild(adv);
+
+const books = document.querySelectorAll(".book");
+books[0].before(books[1]);
+books[2].before(books[4]);
+books[5].after(books[2]);

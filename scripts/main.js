b55cc5870c3fecc728cf3e809f75ce1792e16086
BlackRain <masaraksh86@mail.ru>
2023-03-28 13:30:34 +0700

    Рефакторинг

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,30 @@
-const adv = document.querySelector(".adv");
-
-adv.parentNode.removeChild(adv);
-
 const books = document.querySelectorAll(".book");
-books[0].before(books[1]);
-books[2].before(books[4]);
-books[5].after(books[2]);
 
-const book2 = books[0].childNodes[3].childNodes;
-book2[7].after(book2[13]);
-book2[13].after(book2[17]);
-book2[8].after(book2[14]);
-book2[19].after(book2[5]);
+function removeAdvertisement() {
+    const adv = document.querySelector(".adv");
+    adv.parentNode.removeChild(adv);
+}
 
-const book3text = books[4].childNodes[1].childNodes[1].childNodes[0];
-book3text.textContent = book3text.textContent.replace("попипы", "тотипы");
+function fixBooksOrder() {
+    books[0].before(books[1]);
+    books[2].before(books[4]);
+    books[5].after(books[2]);
+}
+
+function fixBook2() {
+    const book2 = books[0].childNodes[3].childNodes;
+    book2[7].after(book2[13]);
+    book2[13].after(book2[17]);
+    book2[8].after(book2[14]);
+    book2[19].after(book2[5]);
+}
+
+function fixBook3Title() {
+    const book3text = books[4].childNodes[1].childNodes[1].childNodes[0];
+    book3text.textContent = book3text.textContent.replace("попипы", "тотипы");
+}
+
+removeAdvertisement();
+fixBooksOrder();
+fixBook2();
+fixBook3Title();
